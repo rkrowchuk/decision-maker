0a0789a031cd21348dd49a827feca34b2ccc44e5
Ryan Williams <r.d.williams.ca@gmail.com>
2022-03-12 19:59:51 +0000

    made our own route file for polls

--- server.js
+++ server.js
@@ -35,13 +35,12 @@ app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+const pollsRoutes = require("./routes/polls");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+
+app.use("/api/polls", pollsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -49,6 +48,7 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Separate them into separate routes files (see above).
 
 app.get("/", (req, res) => {
+  res.locals.title = "Decision Maker";
   res.render("index");
 });
 
