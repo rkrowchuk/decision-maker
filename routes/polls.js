eaecf8da962e8f9fa4c2acd5be964d3bce4955f1
Kelsi Olstad <kelsiolstad@gmail.com>
2022-03-12 13:49:59 -0700

    add route for voting

--- routes/polls.js
+++ routes/polls.js
@@ -5,13 +5,18 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
     res.locals.title = "new route";
     res.render("index");
+  });
+
+  router.get("/id", (req, res) => {
+    res.locals.title = "voting";
+    res.render("voting");
   });
   return router;
 };
