5baeed887651989d3e29ad22f1e0eb3df01f2197
Ryan Williams <r.d.williams.ca@gmail.com>
2022-03-14 03:30:21 +0000

    I believe I have the rankings changing dynamically and the values being assigned the scores correctly, while keeping track of the input names properly

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,9 +4,26 @@ $(document).ready(() => {
   $(function() {
     $("#sortable").sortable({
       containment: "body",
-      cursor: "move"
+      revert: 100,
+      update: function( event, ui ) {
+        const options = document.getElementsByClassName('option');
+        let rank = 1;
+        let score = 3;
+        for (const option in options) {
+          const currElem = (options[option])
+          if(currElem.classList && currElem.classList.contains('option')) {
+            const inputName = $(currElem).find('input').attr('name');
+            $(currElem).find('span').html(
+              `${rank}
+              <input type="hidden" name="${inputName}" value="${score}">`
+            )
+            rank++;
+            score--;
+            console.log($(currElem).find('span').html());
+          }
+        }
+      }
     });
   });
-
 
 });
