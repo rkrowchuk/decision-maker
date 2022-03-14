6da90271e257760a855eb6be6c06add28ceb4007
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-03-14 14:49:05 -0700

    order the results by highest score

--- routes/polls.js
+++ routes/polls.js
@@ -36,14 +36,34 @@ module.exports = (db) => {
     const poll_id = req.params.id;
 
     return db
-    .query(`SELECT polls.question, polls.answer_1, SUM(submissions.a1_score), polls.answer_2, SUM(submissions.a2_score), polls.answer_3, SUM(submissions.a3_score), polls.answer_4, SUM(submissions.a4_score)
+    .query(`SELECT polls.question, polls.answer_1, SUM(submissions.a1_score) AS a1, polls.answer_2, SUM(submissions.a2_score) AS a2, polls.answer_3, SUM(submissions.a3_score) AS a3, polls.answer_4, SUM(submissions.a4_score) AS a4
     FROM polls
     JOIN submissions ON polls.id = poll_id
-    WHERE polls.id = $1;`, [`${poll_id}`])
+    WHERE polls.id = $1
+    GROUP BY polls.question, polls.answer_1, polls.answer_2, polls.answer_3, polls.answer_4;
+`, [`${poll_id}`])
     .then((data) => {
       const scores = data.rows[0];
       console.log(data.rows[0]);
-      const templateVars = { scores };
+
+      const results = [];
+      const ans1 = {};
+      ans1.option = scores.answer_1;
+      ans1.total = scores.a1;
+      const ans2 = {};
+      ans2.option = scores.answer_2;
+      ans2.total = scores.a2;
+      const ans3 = {};
+      ans3.option = scores.answer_3;
+      ans3.total = scores.a3;
+      const ans4 = {};
+      ans4.option = scores.answer_4;
+      ans4.total = scores.a4;
+      results.push(ans1, ans2, ans3, ans4);
+      results.sort((a, b) => {
+        return b.total - a.total;
+      });
+      const templateVars = { scores, results };
       return res.render("results", templateVars);
     })
     .catch((err) => {
