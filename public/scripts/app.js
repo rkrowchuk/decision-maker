956dd4048a3e5dcb46e4ebac2d311b8544a37258
Ryan Williams <r.d.williams.ca@gmail.com>
2022-03-14 03:46:45 +0000

    I have got the rankings changing dynamically and the score values for input also changing dynamically

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,17 +1,23 @@
   // Client facing scripts here
 $(document).ready(() => {
 
+  // Makes the options draggable and sortable.
   $(function() {
     $("#sortable").sortable({
       containment: "body",
       revert: 100,
+
+      /* Upon the dropping of the grabbed option.
+      This logic updates the visable rankings and input score values of the options. */
       update: function( event, ui ) {
         const options = document.getElementsByClassName('option');
         let rank = 1;
         let score = 3;
+
         for (const option in options) {
           const currElem = (options[option])
-          if(currElem.classList && currElem.classList.contains('option')) {
+
+          if(currElem.classList && currElem.classList.contains('option') && !currElem.classList.contains('ui-sortable-placeholder')) {
             const inputName = $(currElem).find('input').attr('name');
             $(currElem).find('span').html(
               `${rank}
@@ -19,7 +25,9 @@ $(document).ready(() => {
             )
             rank++;
             score--;
-            console.log($(currElem).find('span').html());
+
+            // Commented this console.log out for now, will be used to test if still working correctly if there are changes to the html
+            // console.log($(currElem).find('span').html());
           }
         }
       }
